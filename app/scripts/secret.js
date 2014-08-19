c3c3aab6088cc2a600bf890d853db9aeb1be3dce
thachmle <thachmle@aol.com>
2014-08-19 06:04:50 -0400

    edit error

--- app/scripts/secret.js
+++ app/scripts/secret.js
@@ -1,2 +1,2 @@
-var client_ID='5082356dca28f4359b40'
-var client_Secret= 'a68e54f21f0f7205cd0864fd024bd1440949d6b2'
+var client_ID='5082356dca28f4359b40';
+var client_Secret= 'a68e54f21f0f7205cd0864fd024bd1440949d6b2';
